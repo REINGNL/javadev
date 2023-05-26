5518293e86856c6e42490ca747075b365e3cd5a1
Sirius-13 <cwxdesmond2002@gmail.com>
2023-05-27 03:25:19 +0800

    1. Added labels and textfield for registration page 2. Checking for if staff ID or password in empty or not 3. Set mainPageFrame as not resizeable

--- HMS.java
+++ HMS.java
@@ -8,7 +8,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.GridLayout;
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.Border;
@@ -25,18 +24,11 @@ public class HMS {
         JTextField staffIDField = new JTextField();
         JPasswordField passwordField = new JPasswordField();
 
-        // Remove the border around the text fields
-        staffIDField.setBorder(null);
-        passwordField.setBorder(null);
-
         // Create labels for staff ID and password
-        JTextField staffIDLabel = new JTextField("Staff ID: ");
-        staffIDLabel.setEditable(false);
-        staffIDLabel.setBorder(null);
-        JTextField passwordLabel = new JTextField("Password: ");
-        passwordLabel.setEditable(false);
-        passwordLabel.setBorder(null);
+        JLabel staffIDLabel = new JLabel("Staff ID: ");
+        JLabel passwordLabel = new JLabel("Password: ");
 
+        // Add the components to panel
         panel.add(staffIDLabel);
         panel.add(staffIDField);
         panel.add(passwordLabel);
@@ -54,8 +46,16 @@ public class HMS {
             System.out.println("password: " + password);
             if (!staffID.isEmpty() && !password.isEmpty()) {
                 mainPageFrame(staffID);
+            } else if (staffID.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Pleese enter your staff ID.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                loginPage();
+            } else if (password.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Pleese enter your password.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                loginPage();
             } else {
-                JOptionPane.showMessageDialog(null, "Invalid Staff ID or Password.", "Error",
+                JOptionPane.showMessageDialog(null, "Invalid Login.", "Error",
                         JOptionPane.ERROR_MESSAGE);
                 loginPage();
             }
@@ -67,31 +67,112 @@ public class HMS {
 
     // Main Page for Hospital Management System
     public static void mainPageFrame(String staffID) {
-        // Create a new JFrame for the next page
         JFrame mainPageFrame = new JFrame("Hospital Management System");
 
-        // Create a current user label to display the staff ID
-        JLabel currentUserLabel = new JLabel("Welcome to Hospital Management System!");
-        currentUserLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel systemTitleLabel = new JLabel("Welcome to Hospital Management System!");
+        systemTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // Create an empty border with padding
         Border emptyBorder = BorderFactory.createEmptyBorder(20, 0, 15, 0);
+        systemTitleLabel.setBorder(emptyBorder);
 
-        // Set the empty border on the label
-        currentUserLabel.setBorder(emptyBorder);
-
-        // Create a font with the desired size
-        Font labelFont = currentUserLabel.getFont();
+        Font labelFont = systemTitleLabel.getFont();
         Font newFont = new Font(labelFont.getFontName(), Font.PLAIN, 20);
-        // Set the new font for the label
-        currentUserLabel.setFont(newFont);
+        systemTitleLabel.setFont(newFont);
 
         JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
 
-        // Create panels and contents for each tab
+        // Registration Page
         JPanel registrationPanel = new JPanel();
-        JLabel registrationPanel_1 = new JLabel("This is the registration panel");
-        registrationPanel.add(registrationPanel_1);
+        registrationPanel.setLayout(null);
+        // Font for Registration Page
+        Font FontRegistrationLabel = new Font(labelFont.getFontName(), Font.PLAIN, 15);
+        // Labels for Registration Page
+        JLabel firstNameLabel = new JLabel("First Name: ");
+        firstNameLabel.setBounds(50, 30, 250, 30);
+        firstNameLabel.setFont(FontRegistrationLabel);
+        JLabel lastNameLabel = new JLabel("Last Name: ");
+        lastNameLabel.setBounds(50, 80, 250, 30);
+        lastNameLabel.setFont(FontRegistrationLabel);
+        JLabel phoneNumberLabel = new JLabel("Phone Number: ");
+        phoneNumberLabel.setBounds(50, 130, 250, 30);
+        phoneNumberLabel.setFont(FontRegistrationLabel);
+        JLabel personalHealthNumberLabel = new JLabel("Personal Health Number: ");
+        personalHealthNumberLabel.setBounds(50, 180, 250, 30);
+        personalHealthNumberLabel.setFont(FontRegistrationLabel);
+        JLabel addressLabel = new JLabel("Address: ");
+        addressLabel.setBounds(50, 230, 250, 30);
+        addressLabel.setFont(FontRegistrationLabel);
+        JLabel postalCodeLabel = new JLabel("Postal Code: ");
+        postalCodeLabel.setBounds(50, 280, 250, 30);
+        postalCodeLabel.setFont(FontRegistrationLabel);
+        JLabel cityLabel = new JLabel("City: ");
+        cityLabel.setBounds(50, 330, 250, 30);
+        cityLabel.setFont(FontRegistrationLabel);
+        JLabel countryLabel = new JLabel("Country: ");
+        countryLabel.setBounds(50, 380, 250, 30);
+        countryLabel.setFont(FontRegistrationLabel);
+        JLabel patientIdenficationNumberLabel = new JLabel("Patient Idenfication Number: ");
+        patientIdenficationNumberLabel.setBounds(50, 430, 250, 30);
+        patientIdenficationNumberLabel.setFont(FontRegistrationLabel);
+        // Text Field for Registration Page
+        JTextField firstNameField = new JTextField();
+        firstNameField.setBounds(270, 30, 500, 30);
+        JTextField lastNameField = new JTextField();
+        lastNameField.setBounds(270, 80, 500, 30);
+        JTextField phoneNumberField = new JTextField();
+        phoneNumberField.setBounds(270, 130, 500, 30);
+        JTextField personalHealthNumberField = new JTextField();
+        personalHealthNumberField.setBounds(270, 180, 500, 30);
+        JTextField addressField = new JTextField();
+        addressField.setBounds(270, 230, 500, 30);
+        JTextField postalCodeField = new JTextField();
+        postalCodeField.setBounds(270, 280, 500, 30);
+        JTextField cityField = new JTextField();
+        cityField.setBounds(270, 330, 500, 30);
+        JTextField countryField = new JTextField();
+        countryField.setBounds(270, 380, 500, 30);
+        JTextField patientIdenficationNumberField = new JTextField();
+        patientIdenficationNumberField.setBounds(270, 430, 500, 30);
+        // Button for Registration Page
+        JButton registerButton = new JButton("Register");
+        registerButton.setBounds(330, 510, 200, 30);
+        // Add components to Registration Page
+        registrationPanel.add(firstNameLabel);
+        registrationPanel.add(firstNameField);
+        registrationPanel.add(lastNameLabel);
+        registrationPanel.add(lastNameField);
+        registrationPanel.add(phoneNumberLabel);
+        registrationPanel.add(phoneNumberField);
+        registrationPanel.add(personalHealthNumberLabel);
+        registrationPanel.add(personalHealthNumberField);
+        registrationPanel.add(addressLabel);
+        registrationPanel.add(addressField);
+        registrationPanel.add(postalCodeLabel);
+        registrationPanel.add(postalCodeField);
+        registrationPanel.add(cityLabel);
+        registrationPanel.add(cityField);
+        registrationPanel.add(countryLabel);
+        registrationPanel.add(countryField);
+        registrationPanel.add(patientIdenficationNumberLabel);
+        registrationPanel.add(patientIdenficationNumberField);
+        registrationPanel.add(registerButton);
+
+        // Add an action listener to the register button
+        registerButton.addActionListener(e -> {
+            // Perform registration logic here
+            System.out.println("First Name: " + firstNameField.getText());
+            System.out.println("Last Name: " + lastNameField.getText());
+            System.out.println("Phone Number: " + phoneNumberField.getText());
+            System.out.println("Personal Health Number: " + personalHealthNumberField.getText());
+            System.out.println("Address: " + addressField.getText());
+            System.out.println("Postal Code: " + postalCodeField.getText());
+            System.out.println("City: " + cityField.getText());
+            System.out.println("Country: " + countryField.getText());
+            System.out.println("Patient Idenfication Number: " + patientIdenficationNumberField.getText());
+            mainPageFrame.dispose();
+            mainPageFrame(staffID);
+        });
 
         JPanel consultationPanel = new JPanel();
         JLabel consultationPanel_1 = new JLabel("This is the consultation panel");
@@ -120,20 +201,18 @@ public class HMS {
         tabbedPane.addTab("Check Out", checkoutPanel);
         tabbedPane.addTab("Log Out", logoutPanel);
 
-        // Create a font with the desired size for the tabbed pane
-        Font tabFont = new Font(labelFont.getFontName(), Font.PLAIN, 30);
+        Font tabFont = new Font(labelFont.getFontName(), Font.PLAIN, 18);
         tabbedPane.setFont(tabFont);
 
-        // Add the tabbed pane to the main frame's content pane
         mainPageFrame.getContentPane().setLayout(new BorderLayout());
         mainPageFrame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-        mainPageFrame.getContentPane().add(currentUserLabel, BorderLayout.NORTH);
+        mainPageFrame.getContentPane().add(systemTitleLabel, BorderLayout.NORTH);
 
         // Set the size and close operation for the frame
-        mainPageFrame.setSize(1000, 700);
+        mainPageFrame.setSize(1150, 700);
         mainPageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Display the frame
+        mainPageFrame.setLocationRelativeTo(null);
+        mainPageFrame.setResizable(false);
         mainPageFrame.setVisible(true);
     }
 }
