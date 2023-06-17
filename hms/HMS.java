1b207373a44844d8dcf8f999dc5fbc0c579cb843
Sirius-13 <cwxdesmond2002@gmail.com>
2023-06-17 23:21:56 +0800

    1. Added time clock on overview page. 2. Added doctor and patient availability on overview page. 3. Added new icon for doctor and patient.

--- hms/HMS.java
+++ hms/HMS.java
@@ -6,7 +6,11 @@ Title: Hospital Management System
 
 package hms;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Side;
@@ -20,11 +24,16 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 public class HMS extends Application {
 
@@ -153,8 +162,8 @@ public class HMS extends Application {
         Label systemTitleLabel = new Label("Welcome, " + staffID);
         systemTitleLabel.setStyle("-fx-font-size: 30;");
         systemTitleLabel.setPadding(new Insets(20));
-        systemTitleLabel.setAlignment(Pos.TOP_CENTER);
 
+        // Tab Pane
         TabPane tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         tabPane.setSide(Side.LEFT);
@@ -204,33 +213,174 @@ public class HMS extends Application {
         tabPane.getTabs().addAll(overviewTab, registerTab, consultationTab, checkoutTab, logoutTab);
 
         VBox mainLayout = new VBox();
-        mainLayout.setAlignment(Pos.TOP_CENTER);
+        mainLayout.setAlignment(Pos.TOP_LEFT);
         mainLayout.getChildren().addAll(systemTitleLabel, tabPane);
 
         Scene mainScene = new Scene(mainLayout, 1200, 800);
         mainScene.getStylesheets().add(getClass().getResource("style").toExternalForm());
         mainPageStage.setScene(mainScene);
         mainPageStage.setMaximized(true);
-        // mainPageStage.setResizable(false);
         mainPageStage.show();
     }
 
     static void overviewPage(VBox overviewLayout) {
         // Font
         Font overviewFont_1 = Font.font("Arial", FontWeight.BOLD, 25);
-
-        // Create a Rectangle to represent the card
-        Rectangle card = new Rectangle(200, 100);
-        card.setFill(Color.WHITE);
-        card.setStroke(Color.BLACK);
-        card.setArcWidth(10);
-        card.setArcHeight(10);
+        Font clockFont = Font.font("Arial", FontWeight.BOLD, 40);
 
         // Label of Overview Page
         Label overviewTitle = new Label("Overview");
+        Label timeLabel = new Label();
+        Label dateWeek = new Label();
+
+        // Clock function
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1), event -> {
+                    Date currentDate = new Date();
+
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+                    String time = timeFormat.format(currentDate);
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                    String date = dateFormat.format(currentDate);
+
+                    SimpleDateFormat dayFormat = new SimpleDateFormat("E");
+                    String dayOfWeek = dayFormat.format(currentDate);
+
+                    Platform.runLater(() -> {
+                        timeLabel.setText(time);
+                        dateWeek.setText(date + ", " + dayOfWeek);
+                    });
+                }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+        // Create a Rectangle to represent the card
+        Rectangle card_1 = new Rectangle(250, 100);
+        card_1.setFill(Color.WHITE);
+        card_1.setStroke(Color.BLACK);
+        card_1.setArcWidth(20);
+        card_1.setArcHeight(20);
+
+        Rectangle card_2 = new Rectangle(250, 100);
+        card_2.setFill(Color.WHITE);
+        card_2.setStroke(Color.BLACK);
+        card_2.setArcWidth(20);
+        card_2.setArcHeight(20);
+
+        StackPane cardLayout_1 = new StackPane(card_1);
+        StackPane cardLayout_2 = new StackPane(card_2);
+
+        // Get total of doctors
+        try {
+            // Connect to MySQL database
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT COUNT(*) AS Total_Doctor FROM staff WHERE EMPLOYEETYPE='Doctor'");
+            if (rs.next()) {
+                int total = rs.getInt("Total_Doctor");
+
+                // Create an ImageView to display the image
+                Image img = new Image("assets/icon/doctor.png");
+                ImageView doctorImgView = new ImageView(img);
+                doctorImgView.setFitWidth(50);
+                doctorImgView.setFitHeight(50);
+
+                Label doctorImg = new Label();
+                doctorImg.setGraphic(doctorImgView);
+                Label totalLabel = new Label("Total doctor's");
+                totalLabel.setFont(Font.font("Arial", 15));
+                Label countLabel = new Label("" + total);
+                countLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+
+                VBox doctorBox = new VBox(5);
+                doctorBox.getChildren().addAll(doctorImg, totalLabel, countLabel);
+                doctorBox.setPadding(new Insets(0, 20, 0, 20));
+                cardLayout_1.getChildren().add(doctorBox);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Opps...Something went wrong");
+                alert.showAndWait();
+            }
+            // Clean up resources
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (Exception a) {
+            a.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Error: " + a.getMessage());
+            alert.showAndWait();
+        }
+
+        // Get total of nurses
+        try {
+            // Connect to MySQL database
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT COUNT(*) AS Total_Patient FROM patient");
+            if (rs.next()) {
+                int total = rs.getInt("Total_Patient");
+
+                // Create an ImageView to display the image
+                Image img = new Image("assets/icon/patient.png");
+                ImageView patientImgView = new ImageView(img);
+                patientImgView.setFitWidth(50);
+                patientImgView.setFitHeight(50);
+
+                Label patientImg = new Label();
+                patientImg.setGraphic(patientImgView);
+                Label totalLabel = new Label("Total patient's");
+                totalLabel.setFont(Font.font("Arial", 15));
+                Label countLabel = new Label("" + total);
+                countLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+
+                VBox patientBox = new VBox(5);
+                patientBox.getChildren().addAll(patientImg, totalLabel, countLabel);
+                patientBox.setPadding(new Insets(0, 20, 0, 20));
+                cardLayout_2.getChildren().add(patientBox);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Opps...Something went wrong");
+                alert.showAndWait();
+            }
+            // Clean up resources
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (Exception a) {
+            a.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Error: " + a.getMessage());
+            alert.showAndWait();
+        }
+
+        VBox clockLayout = new VBox(5);
+        clockLayout.setPrefSize(250, 150);
+        clockLayout.setAlignment(Pos.CENTER);
+        clockLayout.getChildren().addAll(timeLabel, dateWeek);
+
+        VBox availabilityShow = new VBox(5);
+        availabilityShow.getChildren().addAll(cardLayout_1, cardLayout_2);
+
+        HBox firstRow = new HBox(20);
+        firstRow.getChildren().addAll(clockLayout, availabilityShow);
 
         overviewTitle.setFont(overviewFont_1);
-        overviewLayout.getChildren().addAll(overviewTitle, card);
+        timeLabel.setFont(clockFont);
+        VBox contentLayout = new VBox(10);
+        contentLayout.getChildren().addAll(overviewTitle, firstRow);
+        overviewLayout.getChildren().add(contentLayout);
     }
 
     static void registerPage(VBox registerLayout) {
@@ -289,6 +439,9 @@ public class HMS extends Application {
         Button registerBtn = new Button("Register");
         registerBtn.setPadding(new Insets(10));
         registerBtn.setFont(registerLabelFont);
+        HBox btnLayout = new HBox(registerBtn);
+        btnLayout.setAlignment(Pos.CENTER_RIGHT);
+        btnLayout.setPadding(new Insets(10));
 
         registerBtn.setOnAction(e -> {
 
@@ -363,10 +516,7 @@ public class HMS extends Application {
                 lastNameField, phoneNumberLabel, phoneNumberField,
                 dateOfBirthLabel, dateOfBirthField, personalHealthNumberLabel, personalHealthNumberField, addressLabel,
                 addressField, postalCodeLabel, postalCodeField, cityLabel, cityField, countryLabel, countryField,
-                patientIdenficationNumberLabel, patientIdenficationNumberField, registerBtn);
-
-        registerBtn.setLayoutX(1000);
-        registerBtn.setLayoutY(1000);
+                patientIdenficationNumberLabel, patientIdenficationNumberField, btnLayout);
     }
 
     static void consultationPage(VBox consultationLayout) {
@@ -456,5 +606,22 @@ public class HMS extends Application {
                 hms.start(loginPageStage);
             }
         });
+    }
+
+    static void getCurrentTime() {
+        Date currentDate = new Date();
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        String time = timeFormat.format(currentDate);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM");
+        String date = dateFormat.format(currentDate);
+
+        SimpleDateFormat dayFormat = new SimpleDateFormat("E");
+        String dayOfWeek = dayFormat.format(currentDate);
+
+        // System.out.println("Time: " + time);
+        // System.out.println("Date: " + date);
+        // System.out.println("Day: " + dayOfWeek);
     }
 }
